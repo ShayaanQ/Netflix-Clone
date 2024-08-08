8dc85749ef40f71f9c10e665caa69e227e8a3e4f
Shayaan Qureshi <136394273+ShayaanQ@users.noreply.github.com>
2024-08-08 18:59:18 -0500

    final

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
 					</a>
 					. The source code is available on{" "}
 					<a
-						href='https://github.com/burakorkmez'
+						href='https://github.com/ShayaanQ/Netflix-Clone'
 						target='_blank'
 						rel='noreferrer'
 						className='font-medium underline underline-offset-4'
